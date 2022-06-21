ecfebc959ebff0d3a090710e33a499e4ba6a4209
R Cultura <roes2207@gmail.com>
2022-06-21 18:17:14 -0500

    Version estable del menu desplegable

--- js/script.js
+++ js/script.js
@@ -37,3 +37,24 @@ window.addEventListener("resize", function(){
     }
 
 });
+
+
+// Funciones para hacerla más dinamico el menu 
+
+let listElements = document.querySelectorAll('.hola');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+
+    })
+});
